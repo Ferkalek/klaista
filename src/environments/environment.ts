687993954ce12f8d9dfc06354b4d705dabb190ec
Anton Ferkalek <aerkalek@mynexuscare.com>
2024-03-26 08:54:15 +0100

    add pwa again

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'http://localhost:3000',
-  baseUrl: 'https://alista.onrender.com',
+  baseUrl: 'http://localhost:3000',
 };
 
 /*
